8b17c99ba8a18d671a2a3f6ee16b293de89220ef
abdelnasserben <benaliabdelnasserpro@gmail.com>
2024-03-26 11:57:31 +0100

    sample refactoring

--- src/main/java/com/dabel/controller/CardController.java
+++ src/main/java/com/dabel/controller/CardController.java
@@ -135,7 +135,7 @@ public class CardController implements PageTitleConfig {
         }
 
 
-        //TODO: update card information and save it
+        //TODO: save card information and save it
         CardDto cardDto = CardDto.builder()
                 .cardType(postCardDto.getCardType())
                 .cardName(postCardDto.getCardName())
@@ -153,7 +153,7 @@ public class CardController implements PageTitleConfig {
 
         //TODO: approve the request
         cardFacadeService.approveRequest(requestId);
-        redirect.addFlashAttribute(Web.MessageTag.SUCCESS, "Application approved successfully !");
+        redirect.addFlashAttribute(Web.MessageTag.SUCCESS, "Request approved successfully !");
 
         return "redirect:" + Web.Endpoint.CARD_REQUEST_ROOT + "/" + requestId;
     }
@@ -170,64 +170,6 @@ public class CardController implements PageTitleConfig {
 
         return "redirect:" + Web.Endpoint.CARD_REQUEST_ROOT + "/" + requestId;
     }
-
-//    @GetMapping(value = Web.Endpoint.EXCHANGE_ROOT + "/{exchangeId}")
-//    public String exchangeDetails(@PathVariable Long exchangeId, Model model) {
-//
-//        ExchangeDto exchange = cardFacadeService.findById(exchangeId);
-//
-//        configPageTitle(model, "Exchange Details");
-//        model.addAttribute("exchange", StatedObjectFormatter.format(exchange));
-//        return Web.View.EXCHANGE_DETAILS;
-//    }
-//
-//    @GetMapping(value = Web.Endpoint.EXCHANGE_INIT)
-//    public String initExchange(Model model, ExchangeDto exchangeDto) {
-//        configPageTitle(model, Web.Menu.Exchange.INIT);
-//
-//        return Web.View.EXCHANGE_INIT;
-//    }
-
-//    @PostMapping(value = Web.Endpoint.EXCHANGE_INIT)
-//    public String initExchange(Model model, @Valid ExchangeDto exchangeDto, BindingResult binding, RedirectAttributes redirect) {
-//
-//        if(binding.hasErrors()) {
-//            configPageTitle(model, Web.Menu.Exchange.INIT);
-//            return Web.View.EXCHANGE_INIT;
-//        }
-//
-//        //TODO: set branch - We'll replace this automatically by user authenticated
-//        BranchDto branchDto = branchFacadeService.findById(1L);
-//        exchangeDto.setBranch(branchDto);
-//
-//        cardFacadeService.init(exchangeDto);
-//
-//        redirect.addFlashAttribute(Web.MessageTag.SUCCESS, "Exchange successfully initiated");
-//        return "redirect:" + Web.Endpoint.EXCHANGE_INIT;
-//    }
-//
-//    @GetMapping(value = Web.Endpoint.EXCHANGE_APPROVE + "/{exchangeId}")
-//    public String approveExchange(@PathVariable Long exchangeId, RedirectAttributes redirect) {
-//
-//        cardFacadeService.approve(exchangeId);
-//        redirect.addFlashAttribute(Web.MessageTag.SUCCESS, "Exchange successfully approved!");
-//
-//        return "redirect:" + Web.Endpoint.EXCHANGE_ROOT + "/" + exchangeId;
-//    }
-//
-//    @PostMapping(value = Web.Endpoint.EXCHANGE_REJECT + "/{exchangeId}")
-//    public String rejectExchange(@PathVariable Long exchangeId, @RequestParam String rejectReason, RedirectAttributes redirect) {
-//
-//        if(rejectReason.isBlank())
-//            redirect.addFlashAttribute(Web.MessageTag.ERROR, "Reject reason is mandatory!");
-//        else {
-//            redirect.addFlashAttribute(Web.MessageTag.SUCCESS, "Exchange successfully rejected!");
-//            cardFacadeService.reject(exchangeId, rejectReason);
-//        }
-//
-//        return "redirect:" + Web.Endpoint.EXCHANGE_ROOT + "/" + exchangeId;
-//    }
-
 
     @Override
     public String[] getMenuAndSubMenu() {
